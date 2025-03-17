a7897808e2ccedca13313978fb03642ffc0bc807
Angela Hornung <angelahornung@outlook.com>
2025-03-17 13:55:03 -0400

    Export tables button added to plugins menu, code needs to be cleaned up

--- Docport.php
+++ Docport.php
@@ -12,28 +12,116 @@
 /* variables & objects */
 require_once(__DIR__ . '/Util/Db/DbTableManager.php');
 
-/* Plugin Activation & Installation Hooks */
-
-function activation() {
-    dp_init();
-}
-
-function deactivation() {
-
-}
-
+/* Plugin Activation & Installation Management Hooks */
 register_activation_hook(__FILE__, 'activation');
 register_deactivation_hook(__FILE__, 'deactivation');
 register_uninstall_hook(__FILE__, 'dp_uninstall');
 
+add_action('admin_post_dp_export_action', 'dp_export_data');
+add_action( 'admin_notices', 'dp_admin_notice' );
+add_action('admin_enqueue_scripts', 'my_plugin_enqueue_admin_scripts');
+
+function activation(): void {
+	dp_init();
+}
+
+function deactivation(): void {
+	dp_un();
+}
+
+function dp_uninstall() {
+	dp_un();
+}
+
 function dp_init() {
     //check db tables
 	try{
+        //make sure tables exist, dbDelta makes sure there are no duplicates
 		$dpTableManager = new DbTableManager();
 		$dpTableManager->initTables();
 	} catch (\Exception $e) {
 		//todo implement cleaner and more proper error reporting
 		var_dump($e->getMessage());
 	}
+}
+
+function dp_un() {
+	try{
+
+	} catch (\Exception $e) {
+		//todo implement cleaner and more proper error reporting
+		var_dump($e->getMessage());
+	}
+}
+
+function dp_export_data() {
+	try{
+		$dpTableManager = new DbTableManager();
+
+		// Output the SQL
+		$dpTableManager->exportTables();
+		exit;
+	} catch (\Exception $e) {
+		//todo implement cleaner and more proper error reporting
+		var_dump($e->getMessage());
+	}
+}
+
+function dp_admin_notice() {
+	//warning notice example
+	/*$class = 'notice notice-warning';
+	$message = __( 'Before uninstalling the DocPort plugin, it is highly recommended to export your data.', 'docport' );
+	$export_url = admin_url('admin-post.php?action=dp_export_action');
+	$button_text = __( 'Export DocPort Data', 'docport' );
+
+	printf( '<div class="%1$s"><p>%2$s <a href="%3$s" class="button button-primary">%4$s</a></p></div>',
+		esc_attr( $class ),
+		esc_html( $message ),
+		esc_url( $export_url ),
+		esc_html( $button_text )
+	);*/
+}
+
+//scratch
+function export_button(): void {
+	$screen = get_current_screen();
+	if ($screen->id !== 'plugins') {
+		return;
+	}
+	?>
+	<script type="text/javascript">
+        jQuery(document).ready(function($) {
+            $('#deactivate-docport').each(function() {
+                //Find deactivate button
+                var targetButton = $('#deactivate-docport');
+
+                if (targetButton) {
+                    var pluginRow = $(this).closest('tr');
+                    var pluginSlug = pluginRow.find('.plugin-title strong').text().toLowerCase().replace(/ /g, '-'); // Get plugin slug
+
+	                console.log(pluginSlug);
+
+                    var buttonHTML = '<a class="" style="margin-left: 5px;" data-plugin-slug="' + pluginSlug + '" href="' + my_plugin_vars.export_url + '">Export Tables</a>';
+                    targetButton.after(buttonHTML);
+                }
+            });
+        });
+	</script>
+	<?php
+}
+add_action('admin_footer', 'export_button');
+
+function my_plugin_enqueue_admin_scripts($hook): void {
+	//example of admin script
+	if ('plugins.php' !== $hook) {
+		return;
+	}
+	//wp_enqueue_style('my-plugin-admin-style', plugin_dir_url(__FILE__) . 'css/admin.css');
+	wp_enqueue_script('my-plugin-admin-script', plugin_dir_url(__FILE__) . '', array('jquery'), '1.0', true);
+	wp_localize_script('my-plugin-admin-script', 'my_plugin_vars', array(
+		'nonce' => wp_create_nonce('my_plugin_custom_action'),
+		'action' => 'my_plugin_custom_action',
+		'export_url' => admin_url('admin-post.php?action=dp_export_action') // Add the URL here
+	));
 }
 ?>
--- Util/Db/DbTableManager.php
+++ Util/Db/DbTableManager.php
@@ -8,11 +8,21 @@ class DbTableManager
     {
         global $wpdb;
 
-        $table_name = self::PREFIX . 'custom_table';
-
         $charset_collate = $wpdb->get_charset_collate();
 
-        $sql = "CREATE TABLE $table_name (
+		//document table
+        $dpTable = "CREATE TABLE dp_documents (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        title varchar(55) DEFAULT '' NOT NULL,
+        path varchar(55) DEFAULT '' NOT NULL,
+        cat_id mediumint(9) NOT NULL,
+        active tinyint(1) DEFAULT '0',
+        create_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        update_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+	    $catTable = "CREATE TABLE dp_document_categories (
         id mediumint(9) NOT NULL AUTO_INCREMENT,
         title varchar(55) DEFAULT '' NOT NULL,
         path varchar(55) DEFAULT '' NOT NULL,
@@ -25,8 +35,56 @@ class DbTableManager
 
         //require once is for dbDelta
         require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        dbDelta($sql, true);
+        dbDelta($dpTable);
+		dbDelta($catTable);
     }
+
+	public function exportTables() : void
+	{
+		global $wpdb;
+		$tables_to_export = array(
+			'dp_documents',
+			'dp_document_categories',
+		);
+
+		$sql = '';
+		foreach ($tables_to_export as $table) {
+			$create_table = $wpdb->get_row("SHOW CREATE TABLE $table", ARRAY_A);
+			$sql .= "-- Table structure for table `$table`\n";
+
+			$sql .= $create_table["Create Table"] . ";\n\n";
+
+			// Get the table data
+			$rows = $wpdb->get_results("SELECT * FROM $table", ARRAY_A);
+			if ($rows) {
+				$column_names = array_keys($rows[0]);
+				$sql .= "-- Dumping data for table `$table`\n";
+				$sql .= "INSERT INTO `$table` (`" . implode("`,`", $column_names) . "`) VALUES\n";
+				$values = array();
+				foreach ($rows as $row) {
+					$value_str = "(";
+					$value_parts = array();
+					foreach ($row as $value) {
+						$value_parts[] = $wpdb->_real_escape_string($value);
+					}
+					$value_str .= "'" . implode("','", $value_parts) . "')";
+					$values[] = $value_str;
+				}
+				$sql .= implode(",\n", $values) . ";\n\n";
+			}
+		}
+
+		// Set headers for download
+		header('Content-Type: application/sql');
+		header('Content-Disposition: attachment; filename="docport_tables_export.sql"');
+
+		echo $sql;
+		exit;
+	}
+
+	public function delTables(){
+		global $wpdb;
+	}
 }
 
 ?>
